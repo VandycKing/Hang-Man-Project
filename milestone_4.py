e2881c7f3d17edfe316a14573a2ae717854f47e5
VandycKing <mygitglobal@gmail.com>
2024-06-04 22:37:16 +0100

    Defines methods for hangman class

--- milestone_4.py
+++ milestone_4.py
@@ -12,3 +12,31 @@ class Hangman:
         word_guessed = ['_' for _ in self.word]
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+
+        if guess in self.word:
+            print(f"Good guess! '{guess}' is in the word.")
+        else:
+            print(f"Sorry, '{guess}' is not in the word. Try again.")
+
+    def ask_for_input(self):
+        guess = input('Enter a single letter: ')
+
+        while True:
+            if not (guess.isalpha() and len(guess) == 1):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+                break
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+                break
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+
+
+word_list = ['apples', 'mangoes', 'guava', 'oranges', 'grapes']
+hang_man_obj = Hangman(word_list)
+hang_man_obj.ask_for_input()
