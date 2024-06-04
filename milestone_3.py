0091fb96ed73e97cbb506d6dba5293bb1837a8ea
VandycKing <mygitglobal@gmail.com>
2024-06-04 20:48:29 +0100

    Asks for input and validate input with guessed word

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,5 @@
+import random
+
 # TODO: Ask user for input and validate it
 
 guess = input('Enter a single letter: ')
@@ -8,3 +10,14 @@ while True:
     else:
         guess = input(
             "Invalid letter. Please, enter a single alphabetical character: ")
+
+
+# TODO: Iteratively check if the input is a valid guess
+
+word_list = ['apples', 'mangoes', 'guava', 'oranges', 'grapes']
+word = random.choice(word_list)
+
+if guess in word:
+    print(f"Good guess! '{guess}' is in the word.")
+else:
+    print(f"Sorry, '{guess}' is not in the word. Try again.")
