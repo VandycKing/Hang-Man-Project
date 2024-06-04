b50b554f27aa82cccd248d5629edde29c6322b1e
VandycKing <mygitglobal@gmail.com>
2024-06-04 21:11:55 +0100

    Converts code to function

--- milestone_3.py
+++ milestone_3.py
@@ -1,23 +1,34 @@
 import random
 
-# TODO: Ask user for input and validate it
-
-guess = input('Enter a single letter: ')
-while True:
-
-    if guess.isalpha() and len(guess) == 1:
-        break
-    else:
-        guess = input(
-            "Invalid letter. Please, enter a single alphabetical character: ")
-
-
 # TODO: Iteratively check if the input is a valid guess
 
 word_list = ['apples', 'mangoes', 'guava', 'oranges', 'grapes']
 word = random.choice(word_list)
 
-if guess in word:
-    print(f"Good guess! '{guess}' is in the word.")
-else:
-    print(f"Sorry, '{guess}' is not in the word. Try again.")
+
+# TODO: Define check_guess function
+
+def check_guess(guess):
+    guess = guess.lower()
+
+    if guess in word:
+        print(f"Good guess! '{guess}' is in the word.")
+    else:
+        print(f"Sorry, '{guess}' is not in the word. Try again.")
+
+
+# TODO: Define ask_for_input function
+
+def ask_for_input():
+    guess = input('Enter a single letter: ')
+    while True:
+        if guess.isalpha() and len(guess) == 1:
+            break
+        else:
+            guess = input(
+                "Invalid letter. Please, enter a single alphabetical character: ")
+    check_guess(guess)
+
+
+# TODO: Call function
+ask_for_input()
