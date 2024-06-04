161495e77810ec3d1cd212af65fe78b66710370b
VandycKing <mygitglobal@gmail.com>
2024-06-04 20:10:23 +0100

    Checks if guess is valid

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,5 @@
 # %%
+
 import random
 
 # TODO: Create a list with your favourite fruits
@@ -8,7 +9,19 @@ word_list = ['apples', 'mangoes', 'guava', 'oranges', 'grapes']
 for i in word_list:
     print(f'The word is: {i}')
 
+# %%
+
 # TODO: Get a word from the word list
 word = random.choice(word_list)
 print('The randomly selected word is {}'.format(word))
+
+
 # %%
+
+# TODO:
+guess = input('Enter a letter: ')
+
+if guess.isalpha() and len(guess) == 1:
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
