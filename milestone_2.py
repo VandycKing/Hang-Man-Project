3351b2d78d7617a87b87131c99b8716f5bd25e35
VandycKing <mygitglobal@gmail.com>
2024-06-04 19:53:15 +0100

    Prints the selected word

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,4 @@
+# %%
 import random
 
 # TODO: Create a list with your favourite fruits
@@ -5,7 +6,9 @@ word_list = ['apples', 'mangoes', 'guava', 'oranges', 'grapes']
 
 # TODO: print items in word_list
 for i in word_list:
-    print(i)
+    print(f'The word is: {i}')
 
 # TODO: Get a word from the word list
 word = random.choice(word_list)
+print('The randomly selected word is {}'.format(word))
+# %%
